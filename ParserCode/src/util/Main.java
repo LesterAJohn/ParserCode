06d4614b7ab708a6873962b20222f424b1330feb
lester <lester@lester-h8-1214>
2016-12-23 14:28:13 -0600

    Original

---
+++ ParserCode/src/util/Main.java
@@ -0,0 +1,29 @@
+package util;
+
+import util.WebConnect;
+
+/*
+Test Code for Library Functions
+ */
+
+/**
+ *
+ * @author lester.john
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // String encValue = Encryption.RSAencrypt("mypassword");
+        String outPut = WebConnect.sendGet("http://www.environmentallights.com/index.php/api/soap/?wsdl");
+
+        // String decValue = Encryption.RSAdecrypt(encValue);
+        // System.out.println("RSAPublicKey :" + Encryption.pubKey().toString());
+        // System.out.println("RSAPrivateKey :" + Encryption.priKey().toString());  
+        // System.out.println("AESKey :" + Encryption.aesKey().toString());
+
+        // System.out.println("Decrypted Value :" + decValue);
+        System.out.println("Webconnect :" + outPut);       
+    	
+    	// (new KeyDB()).createKeyTableEntry(Encryption.priKey(), Encryption.pubKey(), Encryption.aesKey().toString());
+        }
+    }
